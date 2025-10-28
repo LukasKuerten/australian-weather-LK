401bea45f1b893c97ff1e2c9db73a5a5ab9136e5
LukasKuerten <80959121+LukasKuerten@users.noreply.github.com>
2025-10-28 15:32:18 +0000

    changed to green

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "blue") +  labs(title = "Title") +theme_minimal()
+  geom_point(color = "green") +  labs(title = "Title") +theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
